9339c19c23874e799f4aa36b499c42dbad724b22
wonhee418 <cheeky4@naver.com>
2023-02-14 21:59:43 +0900

    no message

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,13 +1,19 @@
-import axios, { AxiosRequestConfig } from "axios";
+import axios, { AxiosRequestConfig, AxiosError } from "axios";
 import { getStorageUser } from "../common/user/userStorage";
 
 const BASE_URL = "http://localhost:8080";
 
 const axiosConfig: AxiosRequestConfig = {
   baseURL: BASE_URL,
-  headers: {
-    Authorization: getStorageUser(),
-  },
 };
 
 export const API = axios.create(axiosConfig);
+API.interceptors.request.use(
+  (config) => {
+    config.headers.Authorization = getStorageUser();
+    return config;
+  },
+  (error: AxiosError): Promise<AxiosError> => {
+    return Promise.reject(error);
+  }
+);
